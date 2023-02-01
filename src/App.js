d7666bbd7b0b226237d627dcc64337f19f6d6a4f
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-01 06:36:38 +0530

    DARK MODE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React, { useState , useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // USING useState
+  const [theme , setTheme ] = useState('light')
+
+  //FUNCTION - TO TOGGLE THEME 
+  const toggleTheme = () => {
+    if(theme === 'light'){
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+   };
+
+   //useEffect 
+   useEffect(() => {
+    document.body.className = theme;
+   },[theme]);
+
   return (
-    <div className="App">
+    //Dynamically adding classname
+    <div className={`app ${theme}`}>
       <header className="App-header">
+
+      //toggle button
+
+      <button onClick={toggleTheme}>TOGGGLE</button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
       </header>
+
     </div>
   );
 }
